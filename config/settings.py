4d76deaca529a68aa8ceaa9ffe23e0bbe2d69796
Pavel Golovanov <publichead@yandex.ru>
2020-08-29 17:58:41 +0300

    modified users app: add sing/log forms, add email verification

--- config/settings.py
+++ config/settings.py
@@ -131,3 +131,12 @@ AUTH_USER_MODEL = "users.User"
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads/")
 
 MEDIA_URL = "/media/"
+
+
+# Email configuration
+
+EMAIL_HOST = "smtp.mailgun.org"
+EMAIL_PORT = "587"
+EMAIL_HOST_USER = os.environ.get("MAILGUN_USERNAME")
+EMAIL_HOST_PASSWORD = os.environ.get("MAILGUN_PASSWORD")
+EMAIL_FROM = "good-guy@sandbox1c7fcd26ff9b4866b10655850472578e.mailgun.org"
--- users/views.py
+++ users/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+# from core import models
+from django.shortcuts import redirect, reverse
+from django.urls import reverse_lazy
 
-# Create your views here.
+# from django.views import View
+from django.views.generic import FormView
+from django.contrib.auth import authenticate, login, logout
+from . import forms, models
+
+
+class LoginView(FormView):
+
+    template_name = "users/login.html"
+    form_class = forms.LoginForm
+    success_url = reverse_lazy("core:home")
+    # initial = {"email": "admin@admin.com"}
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
+
+
+def log_out(request):
+    logout(request)
+    return redirect(reverse("core:home"))
+
+
+class SignUpView(FormView):
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        user.verify_email()
+        return super().form_valid(form)
+
+
+def complete_verification(request, key):
+    try:
+        user = models.User.objects.get(email_secret=key)
+        user.email_verified = True
+        user.email_secret = ""
+        user.save()
+        # TODO: add success message
+    except models.User.DoesNotExist:
+        # TODO: add error message
+        pass
+    return redirect(reverse("core:home"))
